3543c5be48f5fdf380145c500ea5dd3f4601c5ba
ilgiyigitbasi <ilgiyigitbasi@gmail.com>
2020-02-21 22:17:19 +0300

    first

--- src/Components/AddForm.js
+++ src/Components/AddForm.js
@@ -7,6 +7,7 @@ import Modal from "@material-ui/core/Modal";
 import Backdrop from "@material-ui/core/Backdrop";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import makeStyles from "@material-ui/core/styles/makeStyles";
 
 class AddForm extends Component {
     constructor(props) {
@@ -14,10 +15,12 @@ class AddForm extends Component {
         this.state = {
             data: {
                 adi: '',
-                aciklama: ''
+                aciklama: '',
+                date: '',
             }
         }
     };
+
 
     onClose = () => {
         this.props.onClose();
@@ -41,6 +44,16 @@ class AddForm extends Component {
             }
         });
     };
+    handleDateChange = (evt) => {
+        let val = evt.target.value;
+        this.setState({
+            data: {
+                ...this.state.data,
+                date: val,
+            }
+        });
+        console.log(this.state.data.date)
+    };
     sendData = () => {
         this.props.sendDatas(this.state.data);
     };
@@ -63,20 +76,39 @@ class AddForm extends Component {
                             </IconButton></div>
                             <div className="formBody">
                                 <TextField id="outlined-search" label="Görevin Adı" size="small" variant="outlined"
+                                           fullWidth
+                                           style={{margin: '5px'}}
                                            onChange={(val) => this.handleChange(val)}
                                 />
                                 <TextField
                                     size="small"
                                     id="outlined-multiline-static"
                                     label="Açıklama ve Notlar"
+                                    fullWidth
                                     multiline
                                     rows="6"
                                     variant="outlined"
+                                    style={{margin: '5px'}}
                                     onChange={(val) => this.handleChange2(val)}
                                 />
-                                <Button variant="contained" color="primary" onClick={this.sendData}>
-                                    Ekle
-                                </Button>
+                                <TextField
+                                    id="datetime-local"
+                                    label="Next appointment"
+                                    type="datetime-local"
+                                    defaultValue="2017-05-24T10:30"
+                                    fullWidth
+                                    variant="outlined"
+                                    onChange={(val) => this.handleDateChange(val)}
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                />
+                                <div className="todo-footer">
+                                    <Button variant="contained" color="primary" style={{marginTop: '10px'}}
+                                            onClick={this.sendData}>
+                                        Ekle
+                                    </Button>
+                                </div>
                             </div>
                         </Paper>
                     </Fade>
--- src/Components/mainList.js
+++ src/Components/mainList.js
@@ -8,13 +8,18 @@ import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
 import Typography from "@material-ui/core/Typography";
 import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
 import {MdExpandMore} from "react-icons/all";
+import Tab from "@material-ui/core/Tab";
+import Tabs from "@material-ui/core/Tabs";
+import Button from "@material-ui/core/Button";
 
 class MainList extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
             listItem: [],
             open: false,
+            activePage: 1,
 
         }
     }
@@ -29,9 +34,14 @@ class MainList extends Component {
     formItems = (items) => {
         this.setState(prevState => ({listItem: [...this.state.listItem, items]}))
     };
+    deleteItem = (i) => {
+        const listItem = this.state.listItem.filter((todo, todoIndex) => {
+            return todoIndex !== i
+        });
+        this.setState({listItem})
+    };
 
     render() {
-        console.log(this.state.listItem);
         return (
             <div>
                 <Container className='container'>
@@ -42,28 +52,41 @@ class MainList extends Component {
                         </IconButton></div>
                         {this.state.open &&
                         <AddForm onClose={this.onClose} open={this.state.open} sendDatas={this.formItems}/>}
-                        <div className="listBody">
+                        <Tabs value={this.state.activePage}
+                              indicatorColor="primary"
+                              textColor="default"
+                              centered>
+                            <Tab label="Günlük Görevler" value={1} onClick={() => this.setState({activePage: 1})}/>
+                            <Tab label="Aylık Görevler" value={2} onClick={() => this.setState({activePage: 2})}/>
+                            <Tab label="Haftalık Görevler" value={3} onClick={() => this.setState({activePage: 3})}/>
+                        </Tabs>
+                        <div className={this.state.listItem.length === 0 ? "listBody" : "listBodyActive"}>
                             {this.state.listItem.length === 0 &&
                             <div style={{fontSize: '15px', color: 'lightGray'}}>Hiç eklenmiş görev bulunmamaktadır,
                                 lütfen görev ekleyiniz
                             </div>}
-                            <div className="listBodyItem">
-                            {this.state.listItem[0]?.adi && this.state.listItem.map( (item) =>
-                                <ExpansionPanel key={item.adi}>
-                                    <ExpansionPanelSummary
-                                        expandIcon={<MdExpandMore/>}
-                                        aria-controls="panel1a-content"
-                                        id="panel1a-header"
-                                    >
-                                        <Typography>{item.adi}</Typography>
-                                    </ExpansionPanelSummary>
-                                    <ExpansionPanelDetails>
-                                        <Typography>
-                                            {item.aciklama}
-                                        </Typography>
-                                    </ExpansionPanelDetails>
-                                </ExpansionPanel>
-                            )}
+                            <div className={this.state.listItem.length === 0 ? "listBodyItem" : "listBodyItemActive"}>
+                                {this.state.listItem[0]?.adi && this.activePage === 1 && this.state.listItem.map((item, i) =>
+                                    <ExpansionPanel key={item.adi}>
+                                        <ExpansionPanelSummary
+                                            expandIcon={<MdExpandMore/>}
+                                            aria-controls="panel1a-content"
+                                            id="panel1a-header"
+                                        >
+                                            <Typography>{item.adi}</Typography>
+                                        </ExpansionPanelSummary>
+                                        <ExpansionPanelDetails>
+                                            <Typography>
+                                                {item.aciklama}
+                                                {item.date}
+                                            </Typography>
+                                            <div className="todo-footer">
+                                                <Button variant="contained" color="secondary"
+                                                        onClick={() => this.deleteItem(i)}>Sil</Button>
+                                            </div>
+                                        </ExpansionPanelDetails>
+                                    </ExpansionPanel>
+                                )}
                             </div>
                         </div>
                     </Paper>
